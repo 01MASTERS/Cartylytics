474f062c9b651658e9835184bbe88078cc5750e5
01MASTERS <ravisharma09030@gmail.com>
2026-06-27 18:49:14 +0530

    Fix CORS for Netlify

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     DB_PASSWORD: str = ""
     DB_NAME: str = "cartlytics"
 
-    CORS_ORIGINS: List[str] = ["http://localhost:5173", "http://localhost:3000"]
+    CORS_ORIGINS: List[str] = ["*"]
 
     @property
     def DATABASE_URL(self) -> str:
